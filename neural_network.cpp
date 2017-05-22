4cd4daf7775173c12a76f17ed5394e801c8e36b0
Diego Charrez <ddcharrez@gmail.com>
2017-05-22 02:15:06 -0500

    modify weights

--- neural_network.cpp
+++ neural_network.cpp
@@ -9,38 +9,48 @@ using namespace std;
 #define n_layers 2
 #define n_classes 3
 
+#define alpha 0.5
+
 typedef float FF;
 typedef int II;
 typedef vector<vector<FF>> matrix;
-
-
-FF sigmoid_function(FF x){
-    return 1 / ( 1+exp(-x) );
-}
-
-matrix sigmoid(matrix ms){
-    for(unsigned i; i<ms.size(); ++i){
-        for(unsigned j; j<ms[0].size(); ++j){
-            ms[i][j] = sigmoid_function(ms[i][j]);
-        }
-    }
-    return ms;
-}
+typedef vector<FF> vec;
 
 void print_matrix(matrix ms){
     cout << "[ " << ms.size() << " x " << ms[0].size() << " ]" <<endl;
-    for(auto i : ms){
-        for(auto j : i){
-            cout << j << ' ';
+    for(unsigned i=0; i < ms.size(); ++i){
+        for(unsigned j=0; j < ms[0].size(); ++j){
+            cout << ms[i][j] << ' ';
         }
         cout << endl;
     }
 }
 
+void print_vec(vec ms){
+    cout << "[ " << ms.size() << " ]" <<endl;
+    for(unsigned i=0; i < ms.size(); ++i){
+            cout << ms[i] << ' ';
+    }
+    cout << endl;
+}
+
+FF sigmoid_function(FF x){
+    return 1 / ( 1 + exp(-x) );
+}
+
+vec sigmoid(vec ms){
+    vec sig;
+    for(unsigned i=0; i<ms.size(); ++i){
+            sig.push_back( sigmoid_function(ms[i]) );
+    }
+    return sig;
+}
+
+
 matrix create_layer(II n_inputs, II n_neurons){
     matrix layer;
+    vec tmp;
     for(unsigned i=0; i<n_inputs; ++i){
-        vector<FF> tmp;
         for(unsigned j=0; j<n_neurons; ++j){
             tmp.push_back( (FF) rand() / (RAND_MAX) );
         }
@@ -50,14 +60,85 @@ matrix create_layer(II n_inputs, II n_neurons){
     return layer;
 }
 
+vec net(vec input, matrix weights){
+    matrix sub_w ( weights.begin()+1 , weights.end() );
+    vec ms;
+    for(unsigned i=0; i<sub_w[0].size(); ++i){
+        FF tmp = 0.0 + weights[0][i];
+        for(unsigned j=0; j<sub_w.size(); ++j){
+            tmp += input[j] * sub_w[j][i];
+        }
+        ms.push_back(tmp);
+    }
+    return ms;
+}
+
+void modify_weigths(vec out_desired, vec output, matrix output_layer, 
+                    vec out_h1, matrix hidden_layer, vec inputs){
+
+    vec deltas;
+    vec deltas_h1;
+    FF tmp;
+    
+    for(unsigned i = 0; i<out_desired.size(); ++i){
+        deltas.push_back( -(out_desired[i]-output[i])*(1-output[i])*output[i]);
+    }
+    
+    for(unsigned i = 0; i<output_layer.size(); ++i){
+        for(unsigned j=0; j<output_layer[0].size(); ++j){
+            output_layer[i][j] -= alpha * ( deltas[i] * out_h1[j] );
+        }
+    }
+    
+	print_matrix(output_layer);
+	
+    for(unsigned i = 0; i<output_layer.size(); ++i){
+    	tmp = 0.0;
+        for(unsigned j=0; j<output_layer[0].size(); ++j){
+             tmp += deltas[j] * output_layer[i][j]; 
+        }
+        tmp *= out_h1[i]*(1-out_h1[i]);
+        deltas_h1.push_back(tmp);
+    }
+	
+	for(unsigned i = 0; i<hidden_layer.size(); ++i){
+        for(unsigned j=0; j<hidden_layer[0].size(); ++j){
+             hidden_layer[i][j] -= alpha * deltas_h1[i]*inputs[j];
+        }
+        deltas_h1.push_back(tmp);
+    }
+    
+    print_matrix(hidden_layer);
+    
+}
 
 int main(){
-
-    auto ms = create_layer(5,8);
-    auto ra = sigmoid(ms);
-    print_matrix(ms);
-    print_matrix(ra);
     
+    srand(time(NULL));
+    vec i = {5.1,3.5,1.4,0.2};
+    vec o = {1,0,0};
+
+    matrix hidden_1 ( create_layer(5,8));    
+    print_matrix( hidden_1 );
+
+    matrix output ( create_layer(9,3) );
+    print_matrix( output );
+    
+    vec net_h1 ( net(i, hidden_1) );
+    print_vec( net_h1 );
+    
+    vec out_h1 ( sigmoid(net_h1) ) ;
+    print_vec( out_h1 );
+    
+
+    
+    vec net_out ( net(out_h1, output) );
+    print_vec( net_out );
+    
+    vec out ( sigmoid(net_out) ) ;
+    print_vec( out );
+    
+    modify_weigths(o, out, output, out_h1, hidden_1, i);
 
     return 0;
 }
